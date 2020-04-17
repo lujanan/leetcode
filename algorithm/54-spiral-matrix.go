bf973a02ffcc1f52ce4e45ed8731b1c02f67631d
luja <1004343150@qq.com>
2020-04-18 00:10:11 +0800

    feat: 54,66,118,498,724,747

---
+++ algorithm/54-spiral-matrix.go
@@ -0,0 +1,33 @@
+package algorithm
+
+//螺旋矩阵
+//https://leetcode-cn.com/problems/spiral-matrix/
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) <= 0 {
+		return nil
+	}
+
+	var (
+		m, n     = len(matrix), len(matrix[0])
+		dr, dc   = []int{1, 0, -1, 0}, []int{0, 1, 0, -1}
+		c, r, di int
+		result   []int
+		check    [][]bool
+	)
+	check = make([][]bool, m)
+	for i := 0; i < m; i++ {
+		check[i] = make([]bool, n)
+	}
+
+	for i := 0; i < m*n; i++ {
+		result = append(result, matrix[c][r])
+		check[c][r] = true
+
+		if c+dc[di] < 0 || c+dc[di] >= m || r+dr[di] < 0 || r+dr[di] >= n || check[c+dc[di]][r+dr[di]] {
+			di = (di + 1) % 4
+		}
+		c = c + dc[di]
+		r = r + dr[di]
+	}
+	return result
+}
---
+++ algorithm/54-spiral-matrix_test.go
@@ -0,0 +1,43 @@
+package algorithm
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_spiralOrder(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"t1", args{
+			matrix: [][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+			}}, []int{1, 2, 3, 6, 9, 8, 7, 4, 5}},
+		{"t2", args{
+			matrix: [][]int{
+				{1, 2, 3, 4},
+				{5, 6, 7, 8},
+				{9, 10, 11, 12},
+			}}, []int{1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7}},
+		{"t3", args{
+			matrix: [][]int{
+				{2, 5},
+				{8, 4},
+				{0, -1},
+			}}, []int{2, 5, 4, -1, 0, 8}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := spiralOrder(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("spiralOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
