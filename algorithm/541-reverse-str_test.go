28a195bade530916d827d41814b0d9fad6b0d424
lujanan <jianan_lu@126.com>
2021-08-21 19:24:34 +0800

    feat: 541

---
+++ algorithm/541-reverse-str_test.go
@@ -0,0 +1,28 @@
+package algorithm
+
+import (
+	"testing"
+)
+
+func Test_reverseStr(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"t1", args{"abcdefg", 2}, "bacdfeg"},
+		{"t2", args{"abcd", 2}, "bacd"},
+		{"t3", args{"abcdefg", 3}, "cbadefg"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseStr(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("reverseStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
