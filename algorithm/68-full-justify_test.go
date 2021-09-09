06b14dd4bb52afcaac6020eb42e3bf84adb3df99
lujanan <jianan_lu@126.com>
2021-09-09 18:18:28 +0800

    feat: 68

--- algorithm/68-full-justify_test.go
+++ algorithm/68-full-justify_test.go
@@ -66,7 +66,6 @@
 package algorithm
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
@@ -137,19 +136,4 @@ func Test_fullJustify(t *testing.T) {
 			}
 		})
 	}
-}
-
-func checkEqualStringArr(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for k, v := range a {
-		if b[k] != v {
-			fmt.Println("k", k)
-			fmt.Println("a", v)
-			fmt.Println("b", b[k])
-			return false
-		}
-	}
-	return true
 }
