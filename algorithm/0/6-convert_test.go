222060dfffabad0870cbbaff8dc936633a8a9edf
lujanan <jianan_lu@126.com>
2021-10-21 14:52:18 +0800

    feat: 6

---
+++ algorithm/0/6-convert_test.go
@@ -0,0 +1,81 @@
+//将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+//
+// 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+//
+//
+//P   A   H   N
+//A P L S I I G
+//Y   I   R
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+//
+// 请你实现这个将字符串进行指定行数变换的函数：
+//
+//
+//string convert(string s, int numRows);
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "PAYPALISHIRING", numRows = 3
+//输出："PAHNAPLSIIGYIR"
+//
+//示例 2：
+//
+//
+//输入：s = "PAYPALISHIRING", numRows = 4
+//输出："PINALSIGYAHRPI"
+//解释：
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+//
+//
+// 示例 3：
+//
+//
+//输入：s = "A", numRows = 1
+//输出："A"
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 1000
+// s 由英文字母（小写和大写）、',' 和 '.' 组成
+// 1 <= numRows <= 1000
+//
+// Related Topics 字符串 👍 1324 👎 0
+
+package algorithm_0
+
+import "testing"
+
+func Test_convert(t *testing.T) {
+	type args struct {
+		s       string
+		numRows int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"t1", args{"PAYPALISHIRING", 3}, "PAHNAPLSIIGYIR"},
+		{"t2", args{"PAYPALISHIRING", 4}, "PINALSIGYAHRPI"},
+		{"t3", args{"A", 1}, "A"},
+		{"t4", args{"AB", 1}, "AB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convert(tt.args.s, tt.args.numRows); got != tt.want {
+				t.Errorf("convert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
