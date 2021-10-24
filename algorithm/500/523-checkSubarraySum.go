7a61974e545afaf13f974e2ad33567aa3dc5ec68
lujanan <jianan_lu@126.com>
2021-10-24 15:33:52 +0800

    feat: 523

---
+++ algorithm/500/523-checkSubarraySum.go
@@ -0,0 +1,23 @@
+package algorithm_500
+
+// 前缀和
+func checkSubarraySum(nums []int, k int) bool {
+	if len(nums) < 2 {
+		return false
+	}
+	var res = map[int]int{0: -1}
+	for i := 0; i < len(nums); i++ {
+		if i > 0 {
+			nums[i] += nums[i-1]
+		}
+		nums[i] = nums[i] % k
+		if idx, ok := res[nums[i]]; ok {
+			if i-idx >= 2 {
+				return true
+			}
+		} else {
+			res[nums[i]] = i
+		}
+	}
+	return false
+}
