b64e19ad3feec6718f5fa4541675bd6439647930
lujanan <jianan_lu@126.com>
2021-10-27 18:31:16 +0800

    feat: 20

---
+++ algorithm/0/20-isValid.go
@@ -0,0 +1,75 @@
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+//
+// 有效字符串需满足：
+//
+//
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "()"
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "()[]{}"
+//输出：true
+//
+//
+// 示例 3：
+//
+//
+//输入：s = "(]"
+//输出：false
+//
+//
+// 示例 4：
+//
+//
+//输入：s = "([)]"
+//输出：false
+//
+//
+// 示例 5：
+//
+//
+//输入：s = "{[]}"
+//输出：true
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 10⁴
+// s 仅由括号 '()[]{}' 组成
+//
+// Related Topics 栈 字符串 👍 2728 👎 0
+
+package algorithm_0
+
+func isValid(s string) bool {
+	var stack []byte
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '(', '[', '{':
+			stack = append(stack, s[i])
+		default:
+			if len(stack) <= 0 ||
+				(s[i] == ')' && stack[len(stack)-1] != '(') ||
+				(s[i] == ']' && stack[len(stack)-1] != '[') ||
+				(s[i] == '}' && stack[len(stack)-1] != '{') {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
---
+++ algorithm/0/20-isValid_test.go
@@ -0,0 +1,82 @@
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+//
+// 有效字符串需满足：
+//
+//
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "()"
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "()[]{}"
+//输出：true
+//
+//
+// 示例 3：
+//
+//
+//输入：s = "(]"
+//输出：false
+//
+//
+// 示例 4：
+//
+//
+//输入：s = "([)]"
+//输出：false
+//
+//
+// 示例 5：
+//
+//
+//输入：s = "{[]}"
+//输出：true
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 10⁴
+// s 仅由括号 '()[]{}' 组成
+//
+// Related Topics 栈 字符串 👍 2728 👎 0
+
+package algorithm_0
+
+import "testing"
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"t1", args{"()"}, true},
+		{"t2", args{"()[]{}"}, true},
+		{"t3", args{"(]"}, false},
+		{"t4", args{"([)]"}, false},
+		{"t5", args{"{[]}"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.s); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
