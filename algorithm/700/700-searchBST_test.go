a70cfec9acf95765b361e8985c0ad540e2bff5d5
lujanan <jianan_lu@126.com>
2021-11-26 18:24:07 +0800

    feat: 700

---
+++ algorithm/700/700-searchBST_test.go
@@ -0,0 +1,105 @@
+//给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+//
+// 例如，
+//
+//
+//给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//和值: 2
+//
+//
+// 你应该返回如下子树:
+//
+//
+//      2
+//     / \
+//    1   3
+//
+//
+// 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+// Related Topics 树 二叉搜索树 二叉树 👍 209 👎 0
+
+package algorithm_700
+
+import (
+	reflect "reflect"
+	"testing"
+)
+
+func Test_searchBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			"t1",
+			args{
+				&TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 1,
+						},
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+					Right: &TreeNode{
+						Val: 7,
+					},
+				},
+				2,
+			},
+			&TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+		},
+		{
+			"t2",
+			args{
+				&TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 1,
+						},
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+					Right: &TreeNode{
+						Val: 7,
+					},
+				},
+				6,
+			},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchBST(tt.args.root, tt.args.val); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
