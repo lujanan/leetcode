b42f9745ad9a1b4176fb4671f4d12cf33ecd54b2
lujanan <jianan_lu@126.com>
2021-12-15 17:07:01 +0800

    feat: 1446

---
+++ algorithm/1400/1446-maxPower_test.go
@@ -0,0 +1,75 @@
+//给你一个字符串 s ，字符串的「能量」定义为：只包含一种字符的最长非空子字符串的长度。
+//
+// 请你返回字符串的能量。
+//
+//
+//
+// 示例 1：
+//
+// 输入：s = "leetcode"
+//输出：2
+//解释：子字符串 "ee" 长度为 2 ，只包含字符 'e' 。
+//
+//
+// 示例 2：
+//
+// 输入：s = "abbcccddddeeeeedcba"
+//输出：5
+//解释：子字符串 "eeeee" 长度为 5 ，只包含字符 'e' 。
+//
+//
+// 示例 3：
+//
+// 输入：s = "triplepillooooow"
+//输出：5
+//
+//
+// 示例 4：
+//
+// 输入：s = "hooraaaaaaaaaaay"
+//输出：11
+//
+//
+// 示例 5：
+//
+// 输入：s = "tourist"
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 500
+// s 只包含小写英文字母。
+//
+// Related Topics 字符串 👍 90 👎 0
+
+package algorithm_1400
+
+import "testing"
+
+func Test_maxPower(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantMax int
+	}{
+		{"t1", args{"leetcode"}, 2},
+		{"t2", args{"abbcccddddeeeeedcba"}, 5},
+		{"t3", args{"triplepillooooow"}, 5},
+		{"t4", args{"hooraaaaaaaaaaay"}, 11},
+		{"t5", args{"tourist"}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotMax := maxPower(tt.args.s); gotMax != tt.wantMax {
+				t.Errorf("maxPower() = %v, want %v", gotMax, tt.wantMax)
+			}
+		})
+	}
+}
