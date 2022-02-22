c58ddd9f33a2138f3f122f15e534ec4f3bb4a3f1
lujanan <jianan_lu@126.com>
2022-02-22 23:39:37 +0800

    feat: 1994 todo

---
+++ algorithm/1900/1994-numberOfGoodSubsets.go
@@ -0,0 +1,15 @@
+package algorithm_1900
+
+func numberOfGoodSubsets(nums []int) int {
+	// dp[i] = dp[i-1] * 2 + 1
+
+	var nMap = map[int]int{2: 0, 3: 0, 5: 0, 7: 0, 11: 0, 13: 0, 17: 0, 19: 0, 23: 0, 29: 0}
+	var multiply = map[int][]int{6: {2, 3}, 10: {2, 5}, 14: {2, 7}, 15: {3, 5}, 21: {3, 7}, 22: {2, 11}, 26: {2, 13}}
+	var fn = func(n int) []int {
+		if _, ok := multiply[n]; ok {
+			return multiply[n]
+		}
+		return nil
+	}
+
+}
