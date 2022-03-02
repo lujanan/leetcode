2d54d24606572934b1ef8a8c29d8a9fdf9b05d2e
lujanan <jianan_lu@126.com>
2022-03-02 20:41:29 +0800

    feat: 206 todo

--- algorithm/200/206-reverseList.go
+++ algorithm/200/206-reverseList.go
@@ -1,43 +1,43 @@
 //给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
-// 
-// 
-// 
 //
-// 示例 1： 
 //
-// 
+//
+//
+// 示例 1：
+//
+//
 //输入：head = [1,2,3,4,5]
 //输出：[5,4,3,2,1]
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：head = [1,2]
 //输出：[2,1]
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：head = []
 //输出：[]
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 链表中节点的数目范围是 [0, 5000] 
-// -5000 <= Node.val <= 5000 
-// 
 //
-// 
+// 提示：
 //
-// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
-// 
-// 
+//
+// 链表中节点的数目范围是 [0, 5000]
+// -5000 <= Node.val <= 5000
+//
+//
+//
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+//
+//
 // Related Topics 递归 链表 👍 2315 👎 0
 
 package algorithm_200
@@ -49,13 +49,16 @@ package algorithm_200
  *     Next *ListNode
  * }
  */
- func reverseList(head *ListNode) *ListNode {
-	 var res *ListNode
+func reverseList(head *ListNode) *ListNode {
+	var res = head
+	if head != nil {
+		head = head.Next
+	}
 
-	 for head != nil && head.Next != nil {
-		 head = head.Next
-
-	 }
-
-	 return res
+	for head != nil {
+		next := head.Next
+		head.Next = res
+		head = next
+	}
+	return res
 }
