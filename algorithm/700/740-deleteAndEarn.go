65379a60055d5eaa86f9c7ef1b63f73ba0dea25a
lujanan <jianan_lu@126.com>
2022-03-03 23:04:14 +0800

    feat: 740

--- algorithm/700/740-deleteAndEarn.go
+++ algorithm/700/740-deleteAndEarn.go
@@ -5,16 +5,39 @@ import "sort"
 // 740. 删除并获得点数
 // https://leetcode-cn.com/problems/delete-and-earn/
 
+// 转化成打家劫舍问题
 func deleteAndEarn(nums []int) int {
+	var max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	// 耗费空间
+	var mVal int
+	for _, v := range nums {
+		mVal = max(mVal, v)
+	}
+	var tmp = make([]int, mVal+1)
+	for _, v := range nums {
+		tmp[v] += v
+	}
+
+	var dp = [2]int{0, tmp[0]}
+	for i := 1; i < len(tmp); i++ {
+		dp[0], dp[1] = dp[1], max(dp[1], dp[0]+tmp[i])
+	}
+	return max(dp[0], dp[1])
+}
+
+func deleteAndEarn1(nums []int) int {
 	var nMap = make(map[int]int)
 	var tmp []int
 	for _, v := range nums {
-		if _, ok := nMap[v]; ok {
-			nMap[v] += v
-		} else {
-			nMap[v] = v
+		if _, ok := nMap[v]; !ok {
 			tmp = append(tmp, v)
 		}
+		nMap[v] += v
 	}
 	sort.Ints(tmp)
 	var max = func(a, b int) int {
@@ -24,16 +47,17 @@ func deleteAndEarn(nums []int) int {
 		return b
 	}
 
-	var dp = make([][2]int, len(tmp))
+	var dp = [2][2]int{}
 	dp[0] = [2]int{0, nMap[tmp[0]]}
 
 	for i := 1; i < len(tmp); i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][1])
+		dp[1][0] = max(dp[0][0], dp[0][1])
 		if tmp[i]-tmp[i-1] == 1 {
-			dp[i][1] = max(dp[i-1][0]+nMap[tmp[i]], dp[i-1][1])
+			dp[1][1] = max(dp[0][0]+nMap[tmp[i]], dp[0][1])
 		} else {
-			dp[i][1] = dp[i][0] + nMap[tmp[i]]
+			dp[1][1] = dp[1][0] + nMap[tmp[i]]
 		}
+		dp[0] = dp[1]
 	}
-	return dp[len(tmp)-1][1]
+	return dp[0][1]
 }
--- algorithm/700/740-deleteAndEarn_test.go
+++ algorithm/700/740-deleteAndEarn_test.go
@@ -13,6 +13,7 @@ func Test_deleteAndEarn(t *testing.T) {
 	}{
 		{"t1", args{[]int{3, 4, 2}}, 6},
 		{"t2", args{[]int{2, 2, 3, 3, 3, 4}}, 9},
+		{"t3", args{[]int{1}}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
