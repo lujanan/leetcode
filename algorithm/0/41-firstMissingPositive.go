f88d8a307cebd8990a52abe09d15b1a5d25c81c6
lujanan <jianan_lu@126.com>
2022-03-06 22:58:24 +0800

    feat: 41

--- algorithm/0/41-firstMissingPositive.go
+++ algorithm/0/41-firstMissingPositive.go
@@ -6,6 +6,37 @@ package algorithm_0
 // func firstMissingPositive(nums []int) int {
 func firstMissingPositive(orign []int) int {
 	nums := append([]int{}, orign...)
+	// 如果数组中包含 1~n, return n+1
+	// 如果数组中缺少 1~n 中的某个数, return 该数
+	var n = len(nums)
+	// 所有 <=0 转化为 n+1
+	for i := range nums {
+		if nums[i] <= 0 {
+			nums[i] = n + 1
+		}
+	}
+
+	// 所有 <=n 的转为负数
+	for i := range nums {
+		if nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] > 0 {
+			nums[nums[i]-1] = -nums[nums[i]-1]
+		} else if nums[i] < 0 && -nums[i] <= n && nums[-nums[i]-1] > 0 {
+			nums[-nums[i]-1] = -nums[-nums[i]-1]
+		}
+	}
+
+	// 第一个为正数的，下标 +1 即为缺失的数
+	for i := range nums {
+		if nums[i] > 0 {
+			return i + 1
+		}
+	}
+	return n + 1
+}
+
+func firstMissingPositive1(nums []int) int {
+	// func firstMissingPositive(orign []int) int {
+	// nums := append([]int{}, orign...)
 	var n = len(nums)
 	var m = nums[0]
 	for i := 1; i < n; i++ {
