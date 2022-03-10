a61412b60817f6b4f3a6e1064b9f9b8fa927b9b0
lujanan <jianan_lu@126.com>
2022-03-10 18:29:11 +0800

    feat: 5

--- algorithm/0/5-m-longestPalindrome.go
+++ algorithm/0/5-m-longestPalindrome.go
@@ -3,23 +3,75 @@ package algorithm_0
 // 5. 最长回文子串
 // https://leetcode-cn.com/problems/longest-palindromic-substring/
 
+// 暴力，已每个字符为中心，向中间两边延伸搜索最长回文串
+// O(n^2)
 func longestPalindrome(s string) string {
-	var res = string(s[0])
-	var dp = make([]int, len(s))
-	dp[0] = 1
-	for i := 1; i < len(s); i++ {
-		dp[i] = 1
-		if i-dp[i-1]-1 >= 0 && s[i] == s[i-dp[i-1]-1] {
-			dp[i] = dp[i-1] + 2
-		} else if i-2 >= 0 && s[i] == s[i-2] {
-			dp[i] = 3
-		} else if s[i] == s[i-1] {
-			dp[i] = 2
+	var n = len(s)
+	var fn = func(l, r int) (int, int) {
+		for ; l >= 0 && r < n && l <= r && s[l] == s[r]; l, r = l-1, r+1 {
+		}
+		return l + 1, r - 1
+	}
+
+	var begin, ml = 0, 1
+	for i := 0; i < n; i++ {
+		l, r := fn(i, i)
+		if r-l+1 > ml {
+			begin, ml = l, r-l+1
 		}
 
-		if dp[i] > len(res) {
-			res = string(s[i+1-dp[i] : i+1])
+		l, r = fn(i, i+1)
+		if r-l+1 > ml {
+			begin, ml = l, r-l+1
 		}
 	}
-	return res
+	return string(s[begin : begin+ml])
+}
+
+// 动规 O(n^2)
+func longestPalindrome1(s string) string {
+	var n = len(s)
+	if n < 2 {
+		return s
+	}
+
+	var dp = make([][]bool, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]bool, n)
+		dp[i][i] = true
+	}
+	var begin, ml = 0, 1
+
+	// 子串长度
+	for ll := 2; ll <= n; ll++ {
+		for l := 0; l < n; l++ {
+			var r = l + ll - 1 // 右边界
+			if r >= n {
+				break
+			}
+
+			// if s[l] != s[r] {
+			// 	dp[l][r] = false
+
+			// } else {
+			// 	if ll < 3 {
+			// 		dp[l][r] = true
+			// 	} else {
+			// 		dp[l][r] = dp[l+1][r-1]
+			// 	}
+			// }
+
+			// 优化
+			dp[l][r] = s[l] == s[r]
+			if dp[l][r] && ll >= 3 {
+				// 子串是回文串，当前串才能是回文串
+				dp[l][r] = dp[l+1][r-1]
+			}
+
+			if ml < ll && dp[l][r] {
+				begin, ml = l, ll
+			}
+		}
+	}
+	return string(s[begin : begin+ml])
 }
--- algorithm/0/5-m-longestPalindrome_test.go
+++ algorithm/0/5-m-longestPalindrome_test.go
@@ -13,6 +13,10 @@ func Test_longestPalindrome(t *testing.T) {
 	}{
 		{"t1", args{"babad"}, "bab"},
 		{"t2", args{"cbbd"}, "bb"},
+		{"t3", args{"cbbdddd"}, "dddd"},
+		{"t4", args{"cbbdddddddddddd"}, "dddddddddddd"},
+		{"t5", args{"cbbddbbdd"}, "bbddbb"},
+		{"t6", args{"ababababa"}, "ababababa"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
