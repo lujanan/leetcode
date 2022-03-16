7a815837ce6a89ce691785d23c8e387d29863a06
lujanan <jianan_lu@126.com>
2022-03-16 22:11:42 +0800

    feat: 205

---
+++ algorithm/200/205-e-isIsomorphic_test.go
@@ -0,0 +1,26 @@
+package algorithm_200
+
+import "testing"
+
+func Test_isIsomorphic(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"t1", args{"foo", "bar"}, false},
+		{"t2", args{"badc", "baba"}, false},
+		{"t3", args{"paper", "title"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isIsomorphic(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isIsomorphic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
