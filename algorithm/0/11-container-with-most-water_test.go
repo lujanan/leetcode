7ef6442f194ef63c91b8619b70c5cc400e74a5f1
lujanan <jianan_lu@126.com>
2024-12-03 00:00:31 +0800

    feat: 11

--- algorithm/0/11-container-with-most-water_test.go
+++ algorithm/0/11-container-with-most-water_test.go
@@ -14,6 +14,7 @@ func Test_maxArea(t *testing.T) {
 		{"t1", args{height: []int{1, 8, 6, 2, 5, 4, 8, 3, 7}}, 49},
 		{"t2", args{height: []int{1, 2, 3, 4, 5, 6, 7, 8, 9}}, 20},
 		{"t3", args{height: []int{1, 3, 5, 7, 9, 11, 13, 15, 17}}, 36},
+		{"t4", args{height: []int{1, 3, 5, 7, 9, 11, 13, 15, 17}}, 36},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
