7dfea0f30019b8c8764703646646df70713210e0
lujanan <jianan_lu@126.com>
2024-12-03 22:48:53 +0800

    feat:11

--- algorithm/0/11-container-with-most-water.go
+++ algorithm/0/11-container-with-most-water.go
@@ -7,6 +7,36 @@ func maxArea(height []int) int {
 		return 0
 	}
 
+	var minFn = func(i, j int) int {
+		if i > j {
+			return j
+		}
+		return i
+	}
+	var maxFn = func(i, j int) int {
+		if i > j {
+			return i
+		}
+		return j
+	}
+
+	var water int
+	for i, j := 0, len(height)-1; i < j; {
+		water = maxFn(water, (j-i)*minFn(height[i], height[j]))
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return water
+}
+
+func maxAreaV2(height []int) int {
+	if len(height) < 2 {
+		return 0
+	}
+
 	var water, tmp int
 	var left, right = 0, len(height) - 1
 
