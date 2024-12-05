28ba14dd5078ddfbfbf3bde2336e3c71639a6016
lujanan <jianan_lu@126.com>
2024-12-05 23:53:30 +0800

    feat: 22

--- algorithm/0/22-generateParenthesis.go
+++ algorithm/0/22-generateParenthesis.go
@@ -1,6 +1,25 @@
 package algorithm_0
 
 func generateParenthesis(n int) (res []string) {
+	var gen = func(left, right int, str string) {}
+	gen = func(left, right int, str string) {
+		if left == n && right == n {
+			res = append(res, str)
+			return
+		}
+		if left < n {
+			gen(left+1, right, str+"(")
+		}
+		if left > right {
+			gen(left, right+1, str+")")
+		}
+	}
+
+	gen(0, 0, "")
+	return res
+}
+
+func generateParenthesisV2(n int) (res []string) {
 
 	var gen func(left, right int, str string)
 	gen = func(left, right int, str string) {
