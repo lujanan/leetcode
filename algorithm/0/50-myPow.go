c44cc050094a29bf80a701800b96710be6386230
lujanan <jianan_lu@126.com>
2024-12-08 22:49:01 +0800

    feat: 50

--- algorithm/0/50-myPow.go
+++ algorithm/0/50-myPow.go
@@ -4,6 +4,23 @@ func myPow(x float64, n int) float64 {
 	if n == 0 {
 		return 1
 	}
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+
+	var tmp = myPow(x, n>>1)
+	if n%2 == 0 {
+		return tmp * tmp
+	} else {
+		return tmp * tmp * x
+	}
+}
+
+func myPowV2(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
 
 	if n < 0 {
 		n = -n
