e9b0c1557ec4ab5535e5af780af35e643a0ff0ea
lujanan <jianan_lu@126.com>
2024-12-14 18:31:29 +0800

    feat: 3264 todo

---
+++ algorithm/3200/3264-final-array-state-after-k-multiplication-operations-i.go
@@ -0,0 +1,156 @@
+//给你一个整数数组 nums ，一个整数 k 和一个整数 multiplier 。
+//
+// 你需要对 nums 执行 k 次操作，每次操作中：
+//
+//
+// 找到 nums 中的 最小 值 x ，如果存在多个最小值，选择最 前面 的一个。
+// 将 x 替换为 x * multiplier 。
+//
+//
+// 请你返回执行完 k 次乘运算之后，最终的 nums 数组。
+//
+//
+//
+// 示例 1：
+//
+//
+// 输入：nums = [2,1,3,5,6], k = 5, multiplier = 2
+//
+//
+// 输出：[8,4,6,5,6]
+//
+// 解释：
+//
+//
+//
+//
+// 操作
+// 结果
+//
+//
+// 1 次操作后
+// [2, 2, 3, 5, 6]
+//
+//
+// 2 次操作后
+// [4, 2, 3, 5, 6]
+//
+//
+// 3 次操作后
+// [4, 4, 3, 5, 6]
+//
+//
+// 4 次操作后
+// [4, 4, 6, 5, 6]
+//
+//
+// 5 次操作后
+// [8, 4, 6, 5, 6]
+//
+//
+//
+//
+// 示例 2：
+//
+//
+// 输入：nums = [1,2], k = 3, multiplier = 4
+//
+//
+// 输出：[16,8]
+//
+// 解释：
+//
+//
+//
+//
+// 操作
+// 结果
+//
+//
+// 1 次操作后
+// [4, 2]
+//
+//
+// 2 次操作后
+// [4, 8]
+//
+//
+// 3 次操作后
+// [16, 8]
+//
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+// 1 <= k <= 10
+// 1 <= multiplier <= 5
+//
+//
+// Related Topics 数组 数学 模拟 堆（优先队列） 👍 19 👎 0
+
+package algorithm_3200
+
+import "container/heap"
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func getFinalState(nums []int, k int, multiplier int) []int {
+	var state = new(HeapArray)
+	heap.Init(state)
+	for i, v := range nums {
+		state.Push(&numIdx{num: v, idx: i})
+	}
+
+	for i := 0; i < k; i++ {
+		num := state.Pop().(*numIdx)
+		num.num *= multiplier
+		state.Push(num)
+	}
+
+	for i := 0; i < len(nums); i++ {
+		num := state.Pop().(*numIdx)
+		nums[num.idx] = num.num
+	}
+	return nums
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+type numIdx struct {
+	num int
+	idx int
+}
+
+type HeapArray []*numIdx
+
+func (h HeapArray) Len() int {
+	return len(h)
+}
+
+func (h HeapArray) Less(i, j int) bool {
+	if h[i].num == h[j].num {
+		return i < j
+	}
+	return h[i].num < h[j].num
+}
+
+func (h HeapArray) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *HeapArray) Push(num interface{}) {
+	*h = append(*h, num.(*numIdx))
+}
+
+func (h *HeapArray) Pop() interface{} {
+	var l = h.Len()
+	old := *h
+	num := old[l-1]
+	*h = old[:l-1]
+	return num
+}
