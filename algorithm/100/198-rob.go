56fe9740f72c484e847c4ab47cfcc5065ffaf81f
lujanan <jianan_lu@126.com>
2024-12-21 22:45:45 +0800

    feat: 198

--- algorithm/100/198-rob.go
+++ algorithm/100/198-rob.go
@@ -47,8 +47,7 @@ func rob(nums []int) int {
 }
 
 func robV2(nums []int) int {
-	// dp[i][0] = max(dp[i-1][1])
-	// dp[i][1] = max(dp[i-1][1], dp[i-1][0]+nums[i])
+	// dp[i] = max(dp[i-1], dp[i-2] + nums[i])
 	var max = func(a, b int) int {
 		if a > b {
 			return a
