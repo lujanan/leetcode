d18d2c60bad7e47791ef86b2a9b1fd422de698a2
lujanan <jianan_lu@126.com>
2024-12-28 17:49:18 +0800

    feat: 122

--- algorithm/100/122-maxProfit_test.go
+++ algorithm/100/122-maxProfit_test.go
@@ -61,7 +61,7 @@ func Test_maxProfit2(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maxProfit(tt.args.prices); got != tt.want {
+			if got := maxProfit3(tt.args.prices); got != tt.want {
 				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
 			}
 		})
