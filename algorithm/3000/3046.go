76ccf00640b5c515a119a2d7f60fbabb7f662952
lujanan <jianan_lu@126.com>
2024-12-28 11:01:03 +0800

    feat: 3046

---
+++ algorithm/3000/3046.go
@@ -0,0 +1,57 @@
+// 3046. 分割数组
+//给你一个长度为 偶数 的整数数组 nums 。你需要将这个数组分割成 nums1 和 nums2 两部分，要求：
+//
+//
+// nums1.length == nums2.length == nums.length / 2 。
+// nums1 应包含 互不相同 的元素。
+// nums2也应包含 互不相同 的元素。
+//
+//
+// 如果能够分割数组就返回 true ，否则返回 false 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,1,2,2,3,4]
+//输出：true
+//解释：分割 nums 的可行方案之一是 nums1 = [1,2,3] 和 nums2 = [1,2,4] 。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,1,1,1]
+//输出：false
+//解释：分割 nums 的唯一可行方案是 nums1 = [1,1] 和 nums2 = [1,1] 。但 nums1 和 nums2 都不是由互不相同的元素
+//构成。因此，返回 false 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 100
+// nums.length % 2 == 0
+// 1 <= nums[i] <= 100
+//
+//
+// Related Topics 数组 哈希表 计数 👍 18 👎 0
+
+package algorithm_3000
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func isPossibleToSplit(nums []int) bool {
+	var numMap = make(map[int]int)
+	for _, n := range nums {
+		numMap[n]++
+		if numMap[n] > 2 {
+			return false
+		}
+	}
+	return true
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
