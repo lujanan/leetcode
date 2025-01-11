7b71a40898395af3a27c3d7a7a2a17880af403eb
lujanan <jianan_lu@126.com>
2025-01-11 21:50:24 +0800

    feat: 67

---
+++ algorithm/0/67.go
@@ -0,0 +1,29 @@
+// 67. 二进制求和
+
+package algorithm_0
+
+import (
+	"math"
+)
+
+func addBinary(a string, b string) string {
+	var res = make([]byte, int(math.Max(float64(len(a)), float64(len(b))))+1)
+	var ext, ai, bj uint8
+	for i, j, k := len(a)-1, len(b)-1, len(res)-1; k >= 0; i, j, k = i-1, j-1, k-1 {
+		ai, bj = 0, 0
+		if i >= 0 {
+			ai = a[i] - '0'
+		}
+		if j >= 0 {
+			bj = b[j] - '0'
+		}
+		ext += ai + bj
+		res[k] = byte(ext&1) + '0'
+		ext >>= 1
+	}
+
+	if res[0] == '0' {
+		return string(res[1:])
+	}
+	return string(res)
+}
---
+++ algorithm/0/67_test.go
@@ -0,0 +1,27 @@
+// 67. 二进制求和
+
+package algorithm_0
+
+import "testing"
+
+func Test_addBinary(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"t1", args{"11", "1"}, "100"},
+		{"t2", args{"1010", "1011"}, "10101"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addBinary(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("addBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
