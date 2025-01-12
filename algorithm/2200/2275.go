37350cc3dc3af88b3f2ab533c401a3715a4963fa
lujanan <jianan_lu@126.com>
2025-01-12 22:40:55 +0800

    feat: 2275

--- algorithm/2200/2275.go
+++ algorithm/2200/2275.go
@@ -2,7 +2,25 @@
 
 package algorithm_2200
 
+// 数据范围 2^23 < 10^7 < 2^24 ,所以遍历24个位置上1的数量，
+// 找到1数量最多的位置，便是最长的组合
 func largestCombination(candidates []int) int {
+	var res, tmp int
+	for i := 0; i < 24; i++ {
+		tmp = 0
+		for j := 0; j < len(candidates); j++ {
+			if candidates[j]&(1<<i) != 0 {
+				tmp++
+			}
+		}
+		if res < tmp {
+			res = tmp
+		}
+	}
+	return res
+}
+
+func largestCombinationV2(candidates []int) int {
 	var arr = make([]int, 25)
 	var j int
 	for i := 0; i < len(candidates); i++ {
