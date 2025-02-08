9846c3d94bd6b517f46320b44be705f4cb730812
lujanan <jianan_lu@126.com>
2025-02-08 16:38:55 +0800

    feat: 63

--- algorithm/0/63-uniquePathsWithObstacles.go
+++ algorithm/0/63-uniquePathsWithObstacles.go
@@ -1,3 +1,4 @@
+// 63.不同路径II
 //一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
 //
 // 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
@@ -41,6 +42,34 @@
 package algorithm_0
 
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	var dp = make([]int, len(obstacleGrid[0]))
+	for i := 0; i < len(obstacleGrid); i++ {
+		for j := 0; j < len(obstacleGrid[i]); j++ {
+			// 障碍物不可达，赋0
+			if obstacleGrid[i][j] == 1 {
+				dp[j] = 0
+				continue
+			}
+
+			if j == 0 {
+				if i == 0 {
+					dp[j] = 1 // 起点赋值
+				}
+				// 第1列只能从上面方向下来，值不用变
+				continue
+			}
+
+			if i == 0 {
+				dp[j] = dp[j-1] // 第1行只能从左面方向过来，直接取左边的值
+			} else {
+				dp[j] += dp[j-1] // 上面与左边的值相加
+			}
+		}
+	}
+	return dp[len(dp)-1]
+}
+
+func uniquePathsWithObstaclesV2(obstacleGrid [][]int) int {
 	var dp = make([]int, len(obstacleGrid[0])+1)
 	dp[0] = 0
 	for i := 0; i < len(obstacleGrid); i++ {
