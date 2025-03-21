3ae57ee9148d0029a644a4104b0d6b5d846d56ba
lujanan <jianan_lu@126.com>
2025-03-21 17:21:45 +0800

    feat: 2680

--- algorithm/2600/2680.go
+++ algorithm/2600/2680.go
@@ -1,69 +1,71 @@
-// 2680.最大货值
-//给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k 。每一次操作中，你可以选择一个数并将它乘 2 。 
+// 2680.最大或值
+//给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k 。每一次操作中，你可以选择一个数并将它乘 2 。
 //
-// 你最多可以进行 k 次操作，请你返回 nums[0] | nums[1] | ... | nums[n - 1] 的最大值。 
+// 你最多可以进行 k 次操作，请你返回 nums[0] | nums[1] | ... | nums[n - 1] 的最大值。
 //
-// a | b 表示两个整数 a 和 b 的 按位或 运算。 
+// a | b 表示两个整数 a 和 b 的 按位或 运算。
 //
-// 
 //
-// 示例 1： 
 //
-// 
+// 示例 1：
+//
+//
 //输入：nums = [12,9], k = 1
 //输出：30
 //解释：如果我们对下标为 1 的元素进行操作，新的数组为 [12,18] 。此时得到最优答案为 12 和 18 的按位或运算的结果，也就是 30 。
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：nums = [8,1,2], k = 2
 //输出：35
 //解释：如果我们对下标 0 处的元素进行操作，得到新数组 [32,1,2] 。此时得到最优答案为 32|1|2 = 35 。
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 1 <= nums.length <= 10⁵ 
-// 1 <= nums[i] <= 10⁹ 
-// 1 <= k <= 15 
-// 
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10⁵
+// 1 <= nums[i] <= 10⁹
+// 1 <= k <= 15
+//
 //
 // Related Topics 贪心 位运算 数组 前缀和 👍 65 👎 0
 
 package algorithm_2600
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 func maximumOr(nums []int, k int) int64 {
-    n := len(nums)
-    suf := make([]int64, n + 1)
-    for i := n - 1; i >= 0; i-- {
-        suf[i] = suf[i + 1] | int64(nums[i])
-    }
-    var res, pre int64
-    for i := 0; i < n; i++ {
-        res = max(res, pre | (int64(nums[i]) << k) | suf[i + 1])
-        pre |= int64(nums[i])
-    }
-    return res
+	var orSum, multiBits int64
+	for _, x := range nums {
+		multiBits |= int64(x) & orSum
+		orSum |= int64(x)
+	}
+	var res int64
+	for _, x := range nums {
+		res = max(res, (orSum^int64(x))|(int64(x)<<k)|multiBits)
+	}
+	return res
 }
 
-
+// 前缀或 + 后缀或
 func maximumOrV2(nums []int, k int) int64 {
-    var orSum, multiBits int64
-    for _, x := range nums {
-        multiBits |= int64(x) & orSum
-        orSum |= int64(x)
-    }
-    var res int64
-    for _, x := range nums {
-        res = max(res, (orSum ^ int64(x)) | (int64(x) << k) | multiBits)
-    }
-    return res
+	var suf = make([]int64, len(nums)+1)
+	for i := len(nums) - 1; i >= 0; i-- {
+		suf[i] = suf[i+1] | int64(nums[i]) // 后缀或
+	}
+
+	var res, pre int64
+	for i := 0; i < len(nums); i++ {
+		// 只要一个数<<1，后续k-1次都应该选这个数左移
+		// 遍历每个数左移k次，通过 前缀或 和 后缀或 就能快速得到最大或值
+		res = max(res, pre|(int64(nums[i])<<k)|suf[i+1])
+		pre |= int64(nums[i]) // 记录前缀或
+	}
+	return res
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
