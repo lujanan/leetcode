170892ede0651f833a6939fd6a4e8511b7450af6
lujanan <jianan_lu@126.com>
2025-07-21 23:41:39 +0800

    feat: 1957

--- algorithm/1900/1957.go
+++ algorithm/1900/1957.go
@@ -3,17 +3,21 @@
 package algorithm_1900
 
 func makeFancyString(s string) string {
-	if len(s) < 3 {
-		return s
-	}
 	var bin = []byte(s)
 	var res = make([]byte, 0, len(s))
-	res = append(res, bin[0], bin[1])
-	var l1, l2 = bin[0], bin[1]
-	for i := 2; i < len(bin); i++ {
-		if bin[i] != l1 || bin[i] != l2 {
+	var l byte
+	var n = 0
+	for i := 0; i < len(bin); i++ {
+		if l != bin[i] {
 			res = append(res, bin[i])
-			l1, l2 = l2, bin[i]
+			l = bin[i]
+			n = 1
+		} else {
+			if n == 2 {
+				continue
+			}
+			n++
+			res = append(res, bin[i])
 		}
 	}
 
