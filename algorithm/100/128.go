38c2804a4dd053e1c49e95a2366d8887a794b99c
lujanan <jianan_lu@126.com>
2025-07-26 00:08:18 +0800

    feat: 128

--- algorithm/100/128.go
+++ algorithm/100/128.go
@@ -3,15 +3,29 @@
 package algorithm_100
 
 func longestConsecutive(nums []int) int {
-	var res int
-	var maxMap, minMap = make(map[int]int), make(map[int]int)
-
+	var numMap = make(map[int]bool)
 	for _, num := range nums {
-		maxMap[num] = maxMap[num-1] + 1
-		res = max(res, maxMap[num])
-		
-		minMap[num] = minMap[num+1] + 1
-		res = max(res, minMap[num])
+		numMap[num] = true
 	}
-	return res
+	
+	// 1.不存在num-1,则说明num是连续序列的第一个数，开始内循环求序列长度
+	// 2.否则跳过
+	// 所以外循环时间复杂度是O(n)，内循环每个数也只会遍历1次，复杂度也是O(n)
+	// 总的复杂度为O(n)
+	var ans, curLen, curNum int
+	for num := range numMap {
+		if !numMap[num-1] {
+			curLen = 1
+			curNum = num
+
+			for numMap[curNum+1] {
+				curLen++
+				curNum++
+			}
+
+			ans = max(ans, curLen)
+		}
+	}
+
+	return ans
 }
--- algorithm/100/128_test.go
+++ algorithm/100/128_test.go
@@ -14,8 +14,8 @@ func Test_longestConsecutive(t *testing.T) {
 		want int
 	}{
 		{"t1", args{[]int{100, 4, 200, 1, 3, 2}}, 4},
-		// {"t2", args{[]int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}}, 9},
-		// {"t3", args{[]int{1, 0, 1, 2}}, 3},
+		{"t2", args{[]int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}}, 9},
+		{"t3", args{[]int{1, 0, 1, 2}}, 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
