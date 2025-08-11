0d3699b807cb78e19b8b0fab42bcfa86fdd9a86d
lujanan <jianan_lu@126.com>
2025-08-12 00:12:51 +0800

    feat: 2438 todo

--- algorithm/2400/2438.go
+++ algorithm/2400/2438.go
@@ -16,18 +16,23 @@ func productQueries(n int, queries [][]int) []int {
 		}
 		maxNum >>= 1
 	}
-	for i, j := 0, len(powers)-1; i < j; i, j = i+1, j-1 {
-		powers[i], powers[j] = powers[j], powers[i]
+
+	var p = make([]int, len(powers))
+	for i, j := len(powers)-1, 0; i >= 0; i, j = i-1, j+1 {
+		p[j] = powers[i]
+		if j > 0 {
+			p[j] *= p[j-1]
+		}
 	}
 
 	var ans = make([]int, 0, len(queries))
 	var tmp int
 	for i := 0; i < len(queries); i++ {
-		tmp = 1
-		for j := queries[i][0]; j <= queries[i][1] && j < len(powers); j++ {
-			tmp = tmp * powers[j] % 1000000007
+		tmp = p[queries[i][1]]
+		if 0 < queries[i][0] {
+			tmp = tmp / p[queries[i][0]-1]
 		}
-		ans = append(ans, tmp)
+		ans = append(ans, tmp%1000000007)
 	}
 
 	return ans
