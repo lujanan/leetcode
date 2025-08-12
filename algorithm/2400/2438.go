0b13260e74a396c4d4bdca92b537bfd15c50ae57
lujanan <jianan_lu@126.com>
2025-08-12 10:38:06 +0800

    feat: 2438

--- algorithm/2400/2438.go
+++ algorithm/2400/2438.go
@@ -2,26 +2,31 @@
 
 package algorithm_2000
 
+import "math"
+
 func productQueries(n int, queries [][]int) []int {
 	var maxNum = 1
+	var twoNum int
 	for maxNum<<1 <= n {
 		maxNum <<= 1
+		twoNum++
 	}
 
 	var powers []int
 	for n > 0 {
 		if n >= maxNum {
-			powers = append(powers, maxNum)
+			powers = append(powers, twoNum)
 			n -= maxNum
 		}
 		maxNum >>= 1
+		twoNum--
 	}
 
 	var p = make([]int, len(powers))
 	for i, j := len(powers)-1, 0; i >= 0; i, j = i-1, j+1 {
 		p[j] = powers[i]
 		if j > 0 {
-			p[j] *= p[j-1]
+			p[j] += p[j-1]
 		}
 	}
 
@@ -30,9 +35,11 @@ func productQueries(n int, queries [][]int) []int {
 	for i := 0; i < len(queries); i++ {
 		tmp = p[queries[i][1]]
 		if 0 < queries[i][0] {
-			tmp = tmp / p[queries[i][0]-1]
+			tmp -= p[queries[i][0]-1]
 		}
-		ans = append(ans, tmp%1000000007)
+
+		var num = math.Pow(2, float64(tmp))
+		ans = append(ans, int(math.Mod(num, 1000000007)))
 	}
 
 	return ans
