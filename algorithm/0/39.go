9be13a7225e3feedb6e1845295a77ee57f5f9468
lujanan <jianan_lu@126.com>
2025-08-14 15:04:03 +0800

    feat: 39

--- algorithm/0/39.go
+++ algorithm/0/39.go
@@ -3,8 +3,23 @@
 package algorithm_0
 
 func combinationSum(candidates []int, target int) [][]int {
-	var ans [][]int
+	var ans = make([][]int, 0)
+	var searchFn func(nums []int, total, idx int)
+	searchFn = func(nums []int, total, idx int) {
+		if total >= target {
+			if total == target {
+				var tmp = make([]int, len(nums))
+				copy(tmp, nums)
+				ans = append(ans, tmp)
+			}
+			return
+		}
 
+		for i := idx; i < len(candidates); i++ {
+			searchFn(append(nums, candidates[i]), total+candidates[i], i)
+		}
+	}
+	searchFn([]int{}, 0, 0)
 
-    return ans
+	return ans
 }
