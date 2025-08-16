cc8ef334f2c04776588657dc3de0ea40809dc8df
lujanan <jianan_lu@126.com>
2025-08-17 00:32:28 +0800

    feat: 79

--- algorithm/0/79.go
+++ algorithm/0/79.go
@@ -5,14 +5,11 @@ package algorithm_0
 func exist(board [][]byte, word string) bool {
 	var direct = [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 	var checkBoard = make(map[[2]int]bool)
-	var ll = len(word)
-	var check bool
+	var ll, m, n = len(word), len(board), len(board[0])
+	
 	var dfs func(x, y, d, idx int) bool
 	dfs = func(x, y, d, idx int) bool {
-		if x < 0 || x >= len(board) || y < 0 || y >= len(board[0]) || idx >= ll {
-			return false
-		}
-		if checkBoard[[2]int{x, y}] {
+		if board[x][y] != word[idx] || checkBoard[[2]int{x, y}] {
 			return false
 		}
 
@@ -20,36 +17,27 @@ func exist(board [][]byte, word string) bool {
 			return true
 		}
 
-		if board[x][y] == word[idx] {
-			checkBoard[[2]int{x, y}] = true
-			for i := 0; i < 4; i++ {
-				check = dfs(x+direct[(d+i)%4][0], y+direct[(d+i)%4][1], (d+i)%4, idx+1)
-				if check {
-					break
-				}
+		checkBoard[[2]int{x, y}] = true
+		for i := 0; i < 4; i++ {
+			if x+direct[(d+i)%4][0] < 0 || x+direct[(d+i)%4][0] >= m ||
+				y+direct[(d+i)%4][1] < 0 || y+direct[(d+i)%4][1] >= n ||
+				idx+1 >= ll {
+				continue
+			}
+
+			if dfs(x+direct[(d+i)%4][0], y+direct[(d+i)%4][1], (d+i)%4, idx+1) {
+				return true
 			}
 		}
-
 		checkBoard[[2]int{x, y}] = false
-		return check
+		return false
 	}
 
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[i]); j++ {
-			if board[i][j] != word[0] {
-				continue
-			}
-			if board[i][j] == word[0] && ll == 1 {
+			if dfs(i, j, 0, 0) {
 				return true
 			}
-			checkBoard[[2]int{i, j}] = true
-
-			for d := 0; d < 4; d++ {
-				if dfs(i+direct[d][0], j+direct[d][1], d, 1) {
-					return true
-				}
-			}
-			checkBoard[[2]int{i, j}] = false
 		}
 	}
 
