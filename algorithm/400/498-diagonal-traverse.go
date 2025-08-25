f4c936df41f7b10613be131939419b9ef5903791
lujanan <jianan_lu@126.com>
2025-08-25 18:21:46 +0800

    feat: 498

--- algorithm/400/498-diagonal-traverse.go
+++ algorithm/400/498-diagonal-traverse.go
@@ -3,6 +3,36 @@ package algorithm_400
 // 对角线遍历
 // https://leetcode-cn.com/problems/diagonal-traverse/
 func findDiagonalOrder(matrix [][]int) []int {
+	var ans []int
+	var direct = [][]int{{-1, 1}, {1, -1}}
+	var m, n = len(matrix), len(matrix[0])
+	var i, j, d int
+	for k := 0; k < m*n; k++ {
+		ans = append(ans, matrix[i][j])
+		if 0 <= i+direct[d][0] && i+direct[d][0] < m && 0 <= j+direct[d][1] && j+direct[d][1] < n {
+			i, j = i+direct[d][0], j+direct[d][1]
+			continue
+		}
+
+		if d == 0 { // 斜向上
+			if j+1 < n {
+				j++ // 优先往右
+			} else {
+				i++ // 往下
+			}
+		} else { // 斜向下
+			if i+1 < m {
+				i++ // 优先往下
+			} else {
+				j++ // 往右
+			}
+		}
+		d ^= 1
+	}
+	return ans
+}
+
+func findDiagonalOrder1(matrix [][]int) []int {
 	if len(matrix) <= 0 {
 		return nil
 	}
